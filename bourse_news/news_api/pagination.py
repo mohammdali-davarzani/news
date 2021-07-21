2ed54021cc0517e90d08a97652b1f1316e52d18f
mohammadali <moa.davarzani@gmail.com>
2021-07-21 10:53:19 +0430

    update SmallResultsSetPagination class

--- bourse_news/news_api/pagination.py
+++ bourse_news/news_api/pagination.py
@@ -13,6 +13,6 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 1000
 
 class SmallResultsSetPagination(PageNumberPagination):
-    page_size = 1000
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
